24d6bab6023849c6fa7363165783b5f837d78150
David Alejandro Cuastumal Bucheli <79026475+Dcuastumal@users.noreply.github.com>
2022-09-07 20:03:55 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/X1rIMu6HqWbJ1-F7/scene.splinecode');
